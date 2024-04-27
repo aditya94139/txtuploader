64cae49081728ebe650fe29447b95f2eec113e20
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-04-27 07:38:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-API_ID = API_ID = 18618422
+API_ID = API_ID = 23163380
 
-API_HASH = os.environ.get("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
+API_HASH = os.environ.get("API_HASH", "2dca155e786c7db2d295e5b4ab10783b")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6737838348:AAF6nFskxaPdtWD9OVIhnFNZ2BGPoADShaU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7115403757:AAHyzfrieb-faOXDW5lxSdQjM-NKHp9uULg")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6050277919))
+OWNER = int(os.environ.get("OWNER", 5827915041))
 
-LOG = -1002113866398
+LOG = -1001947451128
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6050277919 5890592765").split()):
+    for x in (os.environ.get("ADMINS", "2057410627").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
